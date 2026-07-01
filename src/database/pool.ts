12b7a7a9276658c913459a08140704d0196ae478
BetoxPrograming <Almaw95loggin@outlook.com>
2026-07-01 09:04:00 -0600

    🐛 fix(database): configure Aiven SSL connection

--- src/database/pool.ts
+++ src/database/pool.ts
@@ -4,26 +4,11 @@ import { env } from '../config/env.js'
 
 const { Pool } = pg
 
-export const pool = new Pool(
-    env.database.url
-        ? {
-            connectionString: env.database.url,
-            ssl: env.database.ssl
-                ? {
-                    rejectUnauthorized: false,
-                }
-                : false,
-        }
-        : {
-            host: env.database.host,
-            port: env.database.port,
-            user: env.database.user,
-            password: env.database.password,
-            database: env.database.name,
-            ssl: env.database.ssl
-                ? {
-                    rejectUnauthorized: false,
-                }
-                : false,
-        },
-)
+const connectionString = env.database.url.replace('?sslmode=require', '')
+
+export const pool = new Pool({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+})
